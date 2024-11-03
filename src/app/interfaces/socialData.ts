653d6d5bc8903afcdd6a6cdbd7b043616d824e91
Kennedy <tuks1032@gmail.com>
2024-11-03 20:46:26 +0200

    interfaces and json data added

---
+++ src/app/interfaces/socialData.ts
@@ -0,0 +1,10 @@
+
+export interface SocialData{
+    icon: string,
+    user: string,
+    followers: string,
+    amount: string,
+    arrow_icon: string,
+    arrow_color: string,
+    color: string
+}
---
+++ src/app/interfaces/todayData.ts
@@ -0,0 +1,9 @@
+
+export interface TodayData{
+    icon: string,
+    type: string,
+    amount: string,
+    percentage: number,
+    arrow_icon: string,
+    arrow_color: string
+}
